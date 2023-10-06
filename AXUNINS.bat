3026260bcdcdb94de7860aefc530ef44e84e56f4
Snixf <84739184+snixf@users.noreply.github.com>
2023-10-06 23:36:52 +0300

    Create AXUNINS.bat

---
+++ AXUNINS.bat
@@ -0,0 +1,122 @@
+@echo off
+cls & echo wingetDebloated - A winget bloatware removal tool made in batch & echo.
+
+echo [1/4] Do you want to keep Microsoft Edge? y/n 
+set /p edge="> "
+echo [2/4] Do you want to keep the Camera App? y/n
+set /p camera="> "
+
+set /p enter="Press ENTER twice to start debloating"
+set /p enter2="Press ENTER one more time to start debloating"
+
+cls & echo wingetDebloated - A winget bloatware removal tool made in batch & echo.
+
+winget uninstall cortana
+winget uninstall skype
+
+if /i "%EDGE%" == "n" taskkill /f /im msedge.exe & winget uninstall Microsoft.Edge --accept-source-agreements --silent && winget uninstall Microsoft.EdgeWebView2Runtime --accept-source-agreements
+if /i "%CAMERA%" == "n" winget uninstall Microsoft.WindowsCamera_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Xbox Applications
+: winget uninstall Microsoft.GamingApp_8wekyb3d8bbwe --accept-source-agreements --silent
+: winget uninstall Microsoft.XboxApp_8wekyb3d8bbwe --accept-source-agreements --silent
+: winget uninstall Microsoft.Xbox.TCUI_8wekyb3d8bbwe --accept-source-agreements --silent
+: winget uninstall Microsoft.XboxSpeechToTextOverlay_8wekyb3d8bbwe --accept-source-agreements --silent
+: winget uninstall Microsoft.XboxIdentityProvider_8wekyb3d8bbwe --accept-source-agreements --silent
+: winget uninstall Microsoft.XboxGamingOverlay_8wekyb3d8bbwe --accept-source-agreements --silent
+: winget uninstall Microsoft.XboxGameOverlay_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Groove Music
+winget uninstall Microsoft.ZuneMusic_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Feedback Hub
+winget uninstall Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Microsoft-Tips...
+winget uninstall Microsoft.Getstarted_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: 3D Viewer
+winget uninstall 9NBLGGH42THS --accept-source-agreements --silent
+
+: MS Solitaire
+winget uninstall Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Paint-3D...
+: winget uninstall 9NBLGGH5FV99 --accept-source-agreements --silent
+
+: Weather 
+winget uninstall Microsoft.BingWeather_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Mail / Calendar
+winget uninstall microsoft.windowscommunicationsapps_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Your Phone
+winget uninstall Microsoft.YourPhone_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: People
+winget uninstall Microsoft.People_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: MS Pay 
+winget uninstall Microsoft.Wallet_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: MS Maps
+winget uninstall Microsoft.WindowsMaps_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: OneNote
+winget uninstall Microsoft.Office.OneNote_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: MS Office
+winget uninstall Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Voice Recorder
+winget uninstall Microsoft.WindowsSoundRecorder_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Movies & TV
+winget uninstall Microsoft.ZuneVideo_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Mixed Reality-Portal
+winget uninstall Microsoft.MixedReality.Portal_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Sticky Notes...
+winget uninstall Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Get Help
+winget uninstall Microsoft.GetHelp_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: OneDrive
+winget uninstall Microsoft.OneDrive --accept-source-agreements --silent
+
+: Calculator (reinstall with winget install calculator)
+: winget uninstall Microsoft.WindowsCalculator_8wekyb3d8bbwe --accept-source-agreements --silent
+
+
+: Windows 11 Bloatware
+
+: Microsoft TO Do
+winget uninstall Microsoft.Todos_8wekyb3d8bbwe --accept-source-agreements --silent
+: Power Automate
+winget uninstall Microsoft.PowerAutomateDesktop_8wekyb3d8bbwe --accept-source-agreements --silent
+: Bing News
+winget uninstall Microsoft.BingNews_8wekyb3d8bbwe --accept-source-agreements --silent
+: Microsoft Teams
+winget uninstall MicrosoftTeams_8wekyb3d8bbwe --accept-source-agreements --silent
+: Microsoft Family
+winget uninstall MicrosoftCorporationII.MicrosoftFamily_8wekyb3d8bbwe --accept-source-agreements --silent
+: Quick Assist
+winget uninstall MicrosoftCorporationII.QuickAssist_8wekyb3d8bbwe --accept-source-agreements --silent
+: Third-Party Preinstalled bloat
+winget uninstall disney+ --accept-source-agreements --silent
+winget uninstall Clipchamp.Clipchamp_yxz26nhyzhsrt --accept-source-agreements --silent
+
+echo Would you want to delete other bloatware that is not included?
+echo (Y)es/(N)o
+set /p "yn=>"
+
+if %yn% == y goto yes
+if %yn% == n goto no
+
+echo. & echo Done. Thank you for using this tool.
+pause
+
+:yes
+start 
